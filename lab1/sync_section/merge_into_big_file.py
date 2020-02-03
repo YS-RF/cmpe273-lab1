c6adb3b69b349209bf9f7a4fd8baa80c906fb645
YS-RF <55119865+YS-RF@users.noreply.github.com>
2020-02-03 12:44:43 -0800

    finished sync

--- lab1/sync_section/merge_into_big_file.py
+++ lab1/sync_section/merge_into_big_file.py
@@ -1,60 +1,9 @@
 from pathlib import Path
-#merge sort section
-def merge_sort (A):
-    merge_sort2(A,0,len(A)-1)
 
-def merge_sort2(A,first,last):
-    if first < last:
-        middle = (first + last )//2
-        merge_sort2(A,first,middle)
-        merge_sort2(A,middle+1,last)
-        merge (A,first,middle,last)
-
-def merge (A,first,middle,last):
-    L = A [first:middle+1]
-    R = A [middle+1:last+1]
-
-    L.append(9999)
-    R.append(9999)
-
-    i=j=0
-    for k in range (first,last+1):
-        if L[i]<=R[j]:
-            A[k] = L[i]
-            i+=1
-        else:
-            A[k] = R[j]
-            j+=1
-
-#sort ten files section
-def sort_ten_files():
-    for i in range (1,11):
-        filename_unsort = "unsorted_" + str(i) + ".txt"
-        filename_sorted = "sorted_" + str(i) + ".txt"
-
-        data_folder = Path("input/")
-        file_to_open = data_folder / filename_unsort
-
-        with open(file_to_open,'r') as f:
-            array = []
-
-            while True:  
-                line = f.readline() 
-                if not line: 
-                    break
-                array.append(int(line.strip()))
-            merge_sort(array)
-
-            with open (data_folder / filename_sorted,"w+") as g:
-                for s in array:
-                    g.write(str(s)+"\n")
-
-    print("finish sorting 10 files")
-
-# merge into a big file section
 array_of_opened_file = []
 
 def sync_merge():
+    #FINAL_CONSTANT_IMPORTANT = 10
 
     tdarr = [[0]*10 for i in range(10)]
     folder_name = Path("input/")
@@ -78,6 +27,7 @@ def sync_merge():
         if temp_local_min is None:
             continue
 
+        
         final_output.write(str(tdarr[temp_local_min[1]][0])+"\n")
 
         del tdarr [temp_local_min[1]][0]
@@ -110,7 +60,7 @@ for anything in array_of_opened_file:
     anything.close()
 
 
-
-
-sort_ten_files()
 sync_merge()
+
+
+
